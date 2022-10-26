65bdc35b546b56ff25183ba34764ee4c21515626
Albert Garcia <alberd_beto@hotmail.com>
2022-10-26 04:09:26 +0200

    api

---
+++ src/api/links.js
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "SimOnly",
+    path: "/simonly",
+  },
+  {
+    name: "Phone",
+    path: "/phone",
+  },
+  {
+    name: "Prepaid",
+    path: "/prepaid",
+  },
+  {
+    name: "Service",
+    path: "/service",
+  },
+  {
+    name: "Cat",
+    path: "/cat",
+  },
+];
