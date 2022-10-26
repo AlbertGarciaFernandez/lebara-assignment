4903d75b55acda8839697b0c2693085ec2afd9eb
Albert Garcia <alberd_beto@hotmail.com>
2022-10-26 04:11:32 +0200

    App & routering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/organism/NavBar/NavBar";
+import TrustpilotBar from "./components/organism/TrustpilotBar/TrustpilotBar";
+
+// pages
+
+import SimOnly from "./pages/SimOnly/SimOnly";
+import Prepaid from "./pages/Prepaid/Prepaid";
+import Service from "./pages/Service/Service";
+import Phone from "./pages/Phones/Phone";
+import Cat from "./pages/Cat/Cat";
+import Home from "./pages/Home/Home";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <TrustpilotBar />
+        <section>
+          <Routes>
+            <Route path="simonly" element={<SimOnly />} />
+            <Route path="/" element={<Home />} />
+            <Route path="prepaid" element={<Prepaid />} />
+            <Route path="service" element={<Service />} />
+            <Route path="phone" element={<Phone />} />
+            <Route path="cat" element={<Cat />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
     </div>
   );
 }
