903e9264b5320e9575aa059f30ba8934e5943bb6
Albert Garcia <alberd_beto@hotmail.com>
2022-10-26 04:12:15 +0200

    Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SimOnly from "../SimOnly/SimOnly";
+import Prepaid from "../Prepaid/Prepaid";
+
+function Home() {
+  return (
+    <div>
+      <SimOnly />
+      <Prepaid />
+    </div>
+  );
+}
+
+export default Home;
